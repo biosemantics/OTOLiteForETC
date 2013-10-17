b762d0e30298c615a32a54988427e3ab17b4d241
FengqiongHuang <huangfengq@gmail.com>
2013-10-17 13:45:35 -0700

    oto query

---
+++ OTOLite_part2/src/edu/arizona/sirls/server/oto/QueryOTO.java
@@ -0,0 +1,41 @@
+package edu.arizona.sirls.server.oto;
+
+import java.io.IOException;
+
+import oto.full.OTOClient;
+import oto.full.beans.GlossaryDictionaryEntry;
+
+public class QueryOTO extends AbstractOTOAccessObject {
+
+	private static QueryOTO instance;
+
+	public static QueryOTO getInstance() throws IOException {
+		if (instance == null) {
+			instance = new QueryOTO();
+		}
+		return instance;
+	}
+
+	public QueryOTO() throws IOException {
+		super();
+	}
+
+	/**
+	 * given a triple with optional definition, get existing or generated uuid
+	 * from OTO
+	 * 
+	 * @param term
+	 * @param category
+	 * @param glossaryType
+	 *            : in string
+	 * @param definition
+	 * @return
+	 */
+	public String getUUID(String term, String category, String glossaryType,
+			String definition) {
+		OTOClient otoClient = createOTOClient();
+		return otoClient.insertAndGetGlossaryDictionaryEntry(glossaryType,
+				term, category, definition).getTermID();
+	}
+
+}
